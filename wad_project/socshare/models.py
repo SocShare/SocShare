7a841a8a918f83eccce37f7ef9269e7b980792c9
fork-bombed <ryanjmurphy2016@gmail.com>
2020-03-15 00:57:45 +0000

    Built initial database models

--- wad_project/socshare/models.py
+++ wad_project/socshare/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from django.utils import timezone
 
-# Create your models here.
+class Society(models.Model):
+    name = models.CharField(max_length=128)
+    acronym = models.CharField(max_length=15)
+    slug = models.SlugField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.acronym)
+        super(Society, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Societies'
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    location = models.CharField(max_length=30)
+    ticket_url = models.URLField()
+    views = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
+    society = models.ForeignKey(Society, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    content = models.TextField()
+    name = models.CharField(max_length=30)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content[:20]
