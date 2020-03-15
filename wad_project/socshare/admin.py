adae63940ed0e5ba4244ffab2342434afb6736a0
fork-bombed <57873842+fork-bombed@users.noreply.github.com>
2020-03-15 02:22:16 +0000

    Models & basic templating (#6)
    * Built initial database models
    
    * Added utils for helpful functions
    
    * Created base template for the site

--- wad_project/socshare/admin.py
+++ wad_project/socshare/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from socshare.models import Society, Event, Comment
 
-# Register your models here.
+admin.site.register(Society)
+admin.site.register(Event)
+admin.site.register(Comment)
