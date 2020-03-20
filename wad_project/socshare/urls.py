1690f70e0c9dae8f6c96d9916e44b1c4d849d2d0
Thomas Burnip <tb@tburnip.co.uk>
2020-03-20 10:22:37 +0000

    Tom's Last commit

--- wad_project/socshare/urls.py
+++ wad_project/socshare/urls.py
@@ -7,8 +7,10 @@ app_name = 'socshare'
 urlpatterns = [
     path('', views.events, name='index'),
     path('events/', views.events, name='events'),
+    path('events/<slug:event_slug>', views.event_page, name='event_page'),
     path('calendar/', views.calendar, name='calendar'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('dashboard/', views.dashboard, name='dashboard')
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('profile/<slug:profile_slug>', views.profile, name='profile'),
 ]
--- wad_project/socshare/views.py
+++ wad_project/socshare/views.py
@@ -1,54 +1,32 @@
 from django.shortcuts import render
 
 def events(request):
-    context = {"events":[
-        {
-            "name":"Card Title",
-            "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem risus, suscipit et commodo sed, viverra nec erat. Donec at tellus nec massa elementum posuere ac et turpis. Aliquam tristique lectus at congue fringilla. Donec et nibh eu leo gravida molestie.",
-            "img":"Test",
-            "url":"Test"
-        },
-                {
-            "name":"Card Title",
-            "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem risus, suscipit et commodo sed, viverra nec erat. Donec at tellus nec massa elementum posuere ac et turpis. Aliquam tristique lectus at congue fringilla. Donec et nibh eu leo gravida molestie.",
-            "img":"Test",
-            "url":"Test"
-        },
-                {
-            "name":"Card Title",
-            "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem risus, suscipit et commodo sed, viverra nec erat. Donec at tellus nec massa elementum posuere ac et turpis. Aliquam tristique lectus at congue fringilla. Donec et nibh eu leo gravida molestie.",
-            "img":"Test",
-            "url":"Test"
-        },
-                {
-            "name":"Card Title",
-            "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem risus, suscipit et commodo sed, viverra nec erat. Donec at tellus nec massa elementum posuere ac et turpis. Aliquam tristique lectus at congue fringilla. Donec et nibh eu leo gravida molestie.",
-            "img":"Test",
-            "url":"Test"
-        },
-                {
-            "name":"Card Title",
-            "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem risus, suscipit et commodo sed, viverra nec erat. Donec at tellus nec massa elementum posuere ac et turpis. Aliquam tristique lectus at congue fringilla. Donec et nibh eu leo gravida molestie.",
-            "img":"Test",
-            "url":"Test"
-        },
-                {
-            "name":"Card Title",
-            "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem risus, suscipit et commodo sed, viverra nec erat. Donec at tellus nec massa elementum posuere ac et turpis. Aliquam tristique lectus at congue fringilla. Donec et nibh eu leo gravida molestie.",
-            "img":"Test",
-            "url":"Test"
-        }
-    ]}
+    context = {"pageTitle":"Events","events":[]}
+    temp = {
+                "name":"Card Title",
+                "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem risus, suscipit et commodo sed, viverra nec erat. Donec at tellus nec massa elementum posuere ac et turpis. Aliquam tristique lectus at congue fringilla. Donec et nibh eu leo gravida molestie.",
+                "img":"test.png",
+                "url":"Test"
+            }
+    for i in range(10):
+        context["events"].append(temp)
     return render(request,'socshare/events.html',context=context)
 
 def calendar(request):
-    return render(request,'socshare/events.html')
+    return render(request,'socshare/calendar.html')
 
 def login(request):
-    return render(request,'socshare/events.html')
+    return render(request,'socshare/login.html')
 
 def register(request):
-    return render(request,'socshare/events.html')
+    return render(request,'socshare/register.html')
 
 def dashboard(request):
-    return render(request,'socshare/events.html')
+    return render(request,'socshare/dashboard.html')
+
+def profile(request,profile_slug):
+    context = {"pageTitle":"Events","banner_img_url":"test.png"}
+    return render(request,'socshare/profile.html',context=context)
+
+def event_page(request,event_slug):
+    return render(request,'socshare/event.html')
