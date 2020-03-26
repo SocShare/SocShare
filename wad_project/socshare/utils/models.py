e17973dd947e8096ec2e871a8ac02362aa7b3715
fork-bombed <ryanjmurphy2016@gmail.com>
2020-03-26 13:21:27 +0000

    Added SRC email scraper

--- wad_project/socshare/utils/models.py
+++ wad_project/socshare/utils/models.py
@@ -1,4 +1,5 @@
 import random
+import cfscrape
 
 def random_name():
     adj = [
@@ -14,3 +15,27 @@ def random_name():
         'Seagull','Rabbit','Horse','Bee'
     ]
     return ' '.join([random.choice(adj),random.choice(animals)])
+
+def decode_hexcodes(encoded):
+    # Have to bypass SRC's weak email protection :)
+    while '&' in encoded:
+        loc = encoded.find('&')
+        end = encoded.find(';')+1
+        string = encoded[loc:end]
+        encoded=encoded.replace(string,chr(int(string[2:-1])))
+    return encoded
+
+def find_encoded_email(html):
+    # Unique string identifiers make this easy
+    identify = 'Email: <a href="/cdn-cgi/l/email-protection#'
+    html = html[html.find(identify):]
+    html = html[html.find('">')+2:html.find('</a></span>')]
+    return html
+
+def compare_email(url, email):
+    scraper = cfscrape.create_scraper()
+    r = scraper.get(url)
+    encoded = find_encoded_email(r.text)
+    if email.lower() == decode_hexcodes(encoded).lower():
+        return True
+    return False
