8e92d13cda0386e053a64cd9d2518faad308173e
Thomas Burnip <tb@tburnip.co.uk>
2020-04-01 21:12:04 +0100

    Refactored Tests and implemented fuzzer within the tests already made

--- wad_project/socshare/tests/register.py
+++ wad_project/socshare/tests/register.py
@@ -10,29 +10,29 @@ class CheckRegistration(TestCase):
     """
     def test_register_not_vaild_email(self):
         failure_message = "Account not registered with SRC!"
-        resp = c.post(reverse('socshare:register'),{"email":non_vaild_email,"password":'test',"verify":'test',"name":'This is a test',"acronym":'tiat'})
+        resp = c.post(reverse('socshare:register'),copy_modify(valid_registration,"email",non_valid_email))
         self.assertTrue(resp.context != None and resp.context["alert_msg"] == failure_message,"Accepting Registration with non valid email")
 
     """
     Testing the Registration function accepts vaild registrations and rejects double registrations
     """
     def test_register_vaild_email_twice(self):
-        resp = c.post(reverse('socshare:register'),{"email":vaild_email,"password":'test',"verify":'test',"name":'This is a test',"acronym":'tiat'})
+        resp = c.post(reverse('socshare:register'),valid_registration)
         self.assertTrue(resp.context == None,"Registration not functioning despite having a vaild email")
 
         failure_message = "An account is already registered with this email address!"
-        resp = c.post(reverse('socshare:register'),{"email":vaild_email,"password":'test',"verify":'test',"name":'This is a test',"acronym":'tiat'})
+        resp = c.post(reverse('socshare:register'),valid_registration)
         self.assertTrue(resp.context != None and resp.context["alert_msg"] == failure_message,"Fails to recognise that the email is already in the DB")
 
         # Asserts that the user has been registered
-        self.assertTrue(c.login(username=slugify('This is a test'),password="test"),"Fails to login to the newly registered account")
+        self.assertTrue(login_with_vaild(),"Fails to login to the newly registered account")
 
     """
     Testing that registration function does not accept non matching passwords
     """
     def test_register_vaild_email_without_matching_passwords(self):
         failure_message =  "Passwords do not match!"
-        resp = c.post(reverse('socshare:register'),{"email":vaild_email,"password":'test1',"verify":'test2',"name":'This is a test',"acronym":'tiat'})
+        resp = c.post(reverse('socshare:register'),copy_modify(valid_registration,"verify","test1"))
         self.assertTrue(resp.context != None and resp.context["alert_msg"] == failure_message,"Registration succeeds despite having non matching passwords")
         
 
@@ -40,5 +40,5 @@ class CheckRegistration(TestCase):
         Testing that registration function does not accept null fields
     """
     def test_register_vaild_email_with_nulls(self):
-        suc,k = nulls_fuzzer(reverse('socshare:register'),{"email":vaild_email,"password":'',"verify":'',"name":'',"acronym":''})
+        suc,k = nulls_fuzzer(reverse('socshare:register'),valid_registration)
         self.assertTrue(suc,f"The first field to inccorrectly accept a null was {k}")
--- wad_project/socshare/tests/tests_core.py
+++ wad_project/socshare/tests/tests_core.py
@@ -8,19 +8,30 @@ from django.test import Client
 from django.urls import reverse, resolve
 from django.template.defaultfilters import slugify
 
-vaild_email = "pausegaminglan@gmail.com"
-non_vaild_email = "example@example.co.uk"
-
+valid_email = "pausegaminglan@gmail.com"
+non_valid_email = "example@example.co.uk"
+valid_registration = {"email":valid_email,"password":'test',"verify":'test',"name":'This is a test',"acronym":'tiat'}
+valid_login = {"email":valid_registration["email"],"username":slugify(valid_registration["name"]),"password":valid_registration["password"]}
 c = Client()
 
-def nulls_fuzzer(url,valid_input,required_mask=None):
+def login_with_vaild():
+    return c.login(username=slugify(valid_registration["name"]),password=valid_registration["password"])
+
+def fuzzer(url,valid_input,replacement,required_mask=None):
     for i,k in enumerate(valid_input):
-        test_input = valid_input.copy()
-        test_input[k] = ""
+        test_input = copy_modify(valid_input,k,replacement)
         resp = c.post(url,test_input)
         if not (resp.context != None and resp.context["alert_msg"] != None):
             if required_mask == None:
                 return False,k
             elif required_mask[i] == 1:
                 return False,k
-    return True
+    return True,None
+
+def nulls_fuzzer(url,valid_input,required_mask=None):
+    return fuzzer(url,valid_input,"",required_mask)
+
+def copy_modify(dic,k,v=""):
+    out = dic.copy()
+    out[k] = v
+    return out
