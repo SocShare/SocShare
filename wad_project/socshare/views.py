f501628f902521cd0b69042b9b101b7566b18a63
Alex Tsernoh <foralwork@gmail.com>
2020-04-02 18:06:44 +0300

    aaa

--- wad_project/socshare/views.py
+++ wad_project/socshare/views.py
@@ -52,7 +52,7 @@ def login_page(request):
                 if user.is_active:
                     login(request, user)
                     return redirect(reverse('socshare:events'))
-        return render(request,'socshare/login.html',context={'alert':'danger','alert_msg':'Email/password is incorrect!'})
+        return render(request,'socshare/login.html',context={"title":"Login",'alert':'danger','alert_msg':'Email/password is incorrect!'})
 
     return render(request,'socshare/login.html',context={"title":"Login"})
 
@@ -70,9 +70,9 @@ def register(request):
         if password == verify:
             if settings.PROD or check_email(email):
                 if User.objects.filter(email=email).count()!=0:
-                    return render(request,'socshare/register.html',context={'alert':'warning','alert_msg':'An account is already registered with this email address!'})
+                    return render(request,'socshare/register.html',context={"title":"Registration",'alert':'warning','alert_msg':'An account is already registered with this email address!'})
                 if Society.objects.filter(acronym=acronym).count()!=0:
-                    return render(request,'socshare/register.html',context={'alert':'warning','alert_msg':'Another society already has this acronym!'})
+                    return render(request,'socshare/register.html',context={"title":"Registration",'alert':'warning','alert_msg':'Another society already has this acronym!'})
                 username = slugify(name)
                 user = User.objects.get_or_create(username=username)[0]
                 user.email = email
@@ -84,10 +84,10 @@ def register(request):
                 login(request, user)
                 return redirect(reverse('socshare:events'))
             else:
-                return render(request,'socshare/register.html',context={'alert':'warning','alert_msg':'Account not registered with SRC!'})
+                return render(request,'socshare/register.html',context={"title":"Registration",'alert':'warning','alert_msg':'Account not registered with SRC!'})
         else:
-            return render(request,'socshare/register.html',context={'alert':'danger','alert_msg':'Passwords do not match!'})
-    return render(request,'socshare/register.html')
+            return render(request,'socshare/register.html',context={"title":"Registration",'alert':'danger','alert_msg':'Passwords do not match!'})
+    return render(request,'socshare/register.html',context={"title":"Registration"})
 
 def dashboard(request):
     if request.user.is_authenticated:
