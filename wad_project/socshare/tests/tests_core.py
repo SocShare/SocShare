eb62b9346f2dd580577128efd99e7a166033fba3
Thomas Burnip <tb@tburnip.co.uk>
2020-04-02 11:14:05 +0100

    Nearly Finished event page testing

--- wad_project/socshare/tests/tests_core.py
+++ wad_project/socshare/tests/tests_core.py
@@ -11,8 +11,18 @@ valid_login = {"email":valid_registration["email"],"username":slugify(valid_regi
 valid_event = {"name":"Testing Everything","date":"2020-04-30","time":"20:02","location":"55.87159101963893, -4.28941011428833","url":"https://tsb.bz/r","description":"Because this is a test is now affiliated with the SRC we have decided to through a party where we test everything within the SRC site"}
 c = Client()
 
+def register_with_vaild():
+    c.post(reverse('socshare:register'),valid_registration)
+
+def register_and_login():
+    register_with_vaild()
+    login_with_vaild()
+
 def login_with_vaild():
     return c.login(username=slugify(valid_registration["name"]),password=valid_registration["password"])
+
+def add_event():
+    c.post(reverse('socshare:add_event'),valid_event)
 
 def fuzzer(url,valid_input,replacement,required_mask=None):
     for k in valid_input:
@@ -32,3 +42,14 @@ def copy_modify(dic,k,v=""):
     out = dic.copy()
     out[k] = v
     return out
+
+def are_all_elements_present(s,dic,mask=None):
+    for k in dic:
+        if dic[k] not in str(s):
+            if mask != None:
+                if mask.get(k,1) == 1:
+                    return False,k
+            else:
+                return False,k
+                
+    return True,""
