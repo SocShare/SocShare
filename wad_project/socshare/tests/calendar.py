020f8021883cc23316056db1bacc966eadd3adab
Thomas Burnip <tb@tburnip.co.uk>
2020-04-02 16:08:38 +0100

    Added a tests for calendar which needs a merge with master in order to complete

--- wad_project/socshare/tests/calendar.py
+++ wad_project/socshare/tests/calendar.py
@@ -4,4 +4,12 @@ from socshare.tests.tests_core import *
 Testing the Calendar Page
 """
 class Calendar(TestCase):
-    pass
+    """
+    Test the Calendar page's ability to render events
+    """
+    def test_calendar_page(self):
+        register_and_login()
+        add_event()
+        resp = c.get(reverse('socshare:calendar'))
+        suc,k = are_all_elements_present(resp.content,valid_event,{"date":0,"time":0,"location":0})
+        self.assertTrue(suc,f"Some info about the event is not shown : {k}")
